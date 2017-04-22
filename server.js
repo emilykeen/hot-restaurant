39401b8527172116c7c777ebfd47d8b26a3a9fc9
Emily Keen <emily.keen711@gmail.com>
2017-04-22 14:00:45 -0400

    test

--- server.js
+++ server.js
@@ -24,16 +24,16 @@ var customers = [];
 var waitlist = [];
 
 app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
+ 
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/reserve", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
+  
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 app.get("/tables", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
+  
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
